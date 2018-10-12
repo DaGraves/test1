cc6715b6bc67077b8f1f4170e1474374b9396967
David <davidgraves831@gmail.com>
2018-10-12 08:27:41 -0700

    initial

--- src/components/app.js
+++ src/components/app.js
@@ -2,21 +2,27 @@ import React from 'react';
 import IncrementButton from './increment-button';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
+constructor(props) {
+    super(props);
+    this.state = {
+        count: 0
+    };
+}
 
-    render() {
-        return (
-            <div className="parent">
-                <div className="count">
-                    Number of clicks: {this.state.count}
-                </div>
-                <IncrementButton />
+increment(){
+    this.setState({
+        count:this.state.count+1
+    })
+}
+
+render() {
+    return (
+        <div className="parent">
+            <div className="count">
+                Number of clicks: {this.state.count}
             </div>
-        );
-    }
+            <IncrementButton count={() => this.increment()}/>
+        </div>
+    );
+}
 }
